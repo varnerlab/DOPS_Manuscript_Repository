92a34470b95880dc412eeb1ef855df99054cf933
Rachel LeCover <rlecover@purdue.edu>
2017-12-14 11:11:53 -0500

    more reorg

---
+++ DOPS/ExpDOPS_core/fit.m
@@ -0,0 +1,38 @@
+%  ----------------------------------------------------------------------------------- 
+%  Copyright (c) 2016 Varnerlab
+%  School of Chemical and Biomolecular Engineering
+%  Cornell University, Ithaca NY 14853 USA
+%
+%----------------*****  Code Author Information *****----------------------
+%   Code Author (Implementation Questions, Bug Reports, etc.): 
+%       Adithya Sagar: asg242@cornell.edu
+% 
+%  Permission is hereby granted, free of charge, to any person obtaining a copy
+%  of this software and associated documentation files (the "Software"), to deal
+%  in the Software without restriction, including without limitation the rights
+%  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%  copies of the Software, and to permit persons to whom the Software is
+%  furnished to do so, subject to the following conditions:
+
+%  The above copyright notice and this permission notice shall be included
+%  in
+%  all copies or substantial portions of the Software.
+%  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%  THE SOFTWARE.
+%  ----------------------------------------------------------------------------------- 
+
+%% Returns a numerical error value to DOPS 
+function fitness = fit(x, optFunction)
+        %optFunction = function to be minimized, x parameters
+        optFuncStr = func2str(optFunction);
+       % if(contains(optFuncStr, 'b4'))
+        %    fitness=optFunction(x);      %An example here is ackley function. This function is user defined an basically determines the error between simulation and experiment. 
+        %else
+            fitness=optFunction(x');
+        %end
+end
